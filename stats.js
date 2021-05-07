920ae0e17a05791847228eefa2830f3639cbcac2
Fellipe Augusto <fellipe.augusto1002@gmail.com>
2021-05-07 00:39:33 -0300

    show system memory status info every second

---
+++ stats.js
@@ -0,0 +1,24 @@
+// Show system memory info every 1s
+
+const os = require('os')
+
+setInterval(() => {
+    const { freemem, totalmem } = os
+
+    const mem = parseInt(freemem() /1024 /1024)
+    const total = parseInt(totalmem() /1024 /1024)
+    const usedmem = total - mem
+    const percents = parseInt((1 - (mem / total)) * 100)
+    
+    const stats = {
+        free: `${mem} MB`,
+        used: `${usedmem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    }
+    
+    console.clear()
+    console.log("######## PC STATS ########")
+    console.table(stats)
+}, 1000);
+
